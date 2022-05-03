1edb61d2a692269088bc8077277c96f566ebdb4c
mohans-1136 <99773838+mohans-1136@users.noreply.github.com>
2022-05-03 16:22:02 +0530

    Update DemoCs.cs

--- DemoCs.cs
+++ DemoCs.cs
@@ -2,9 +2,10 @@ using System;
 namespace Demo {
 	class Calculator{
 		static void Main(string[] args) {
-			string str = "88*6-9+8/2-10";
+			Console.WriteLine("Enter a Valid String for Calculation ");
+			string str = Console.ReadLine();
 			string output = calculate(calculate(calculate(calculate(str, '/'), '*'), '+'), '-');
-			Console.WriteLine(output);
+			Console.WriteLine("Here Your Answer is : "+ output);
 			//function
 			static string calculate(string str, char op) {
 				string firstText = "", secondText = "", replacedStrFirst = "", replacedStrSecond = "",
@@ -12,25 +13,21 @@ namespace Demo {
 				double firstNum = 0, secondNum = 0, result = 0;
 				if(str.IndexOf(op, 1) != -1) {
 					firstText = str.Substring(0, str.IndexOf(op,1));
-					//Substr(Display, Index(Display, Operation) , Length(Display) - Index(Display, Operation) - 1)
 					secondText = str.Substring(str.IndexOf(op) +1, str.Length - str.IndexOf(op)-1);
 					replacedStrFirst = firstText.Substring(0,1)+((((firstText.Substring(1)).Replace("+", "$")).Replace("-", "$")).Replace("*", "$")).Replace("/", "$");
 					replacedStrSecond = (((secondText.Replace("+", "$")).Replace("-", "$")).Replace("*", "$")).Replace("/", "$");
-					if (op == '+' || op == '-')
-                    {
+					if (op == '+' || op == '-'){
 						if (replacedStrFirst.LastIndexOf("$") != -1)
 							firstNumText = firstText.Substring(replacedStrFirst.LastIndexOf("$"), replacedStrFirst.Length - replacedStrFirst.LastIndexOf("$") );
 						else
 							firstNumText = replacedStrFirst;
 					}
-					else
-                    {
+					else{
 						if (replacedStrFirst.LastIndexOf("$") != -1)
 							firstNumText = firstText.Substring(replacedStrFirst.LastIndexOf("$") + 1, replacedStrFirst.Length - replacedStrFirst.LastIndexOf("$") - 1);
 						else
 							firstNumText = replacedStrFirst;
 					}
-					
 					if (replacedStrSecond.IndexOf("$") != -1)
 						SecondNumText = secondText.Substring(0, replacedStrSecond.IndexOf("$"));
 					else
@@ -48,15 +45,13 @@ namespace Demo {
 						remFirstStr=firstText.Substring(0, replacedStrFirst.LastIndexOf("$"));
                     if (replacedStrSecond.IndexOf("$") != -1)
                         remSecStr=secondText.Substring(replacedStrSecond.IndexOf("$"), replacedStrSecond.Length - replacedStrSecond.IndexOf("$"));
-					if (op == '+' || op == '-')
-                    {
+					if (op == '+' || op == '-'){
 						if (result < 0)
 							str = remFirstStr + result + remSecStr;
 						else
 							str = remFirstStr + "+" + result + remSecStr;
 					}
-					else
-                    {
+					else{
 						if (replacedStrFirst.IndexOf("$") != -1)
 							str = remFirstStr + firstText.Substring(replacedStrFirst.LastIndexOf("$"), 1) + result + remSecStr;
 						else
